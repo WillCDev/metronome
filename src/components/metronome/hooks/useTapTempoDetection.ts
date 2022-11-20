c297fddce16e841bf3427b919d637d32c5321e2a
William Chadwick <william_chadwick@external.mckinsey.com>
2022-11-20 17:48:58 +0000

    feat: add beat detection

---
+++ src/components/metronome/hooks/useTapTempoDetection.ts
@@ -0,0 +1,84 @@
+import { useEffect, useRef, useState } from 'react'
+import { MIN_BPM } from '../Metronome.config'
+
+const NUM_OF_INTERVALS_FOR_DETECTION = 4
+const ONE_MINUTE = 60000
+const MAX_WAIT_AFTER_LAST_TAP = ONE_MINUTE / MIN_BPM
+const MAX_WAIT_BETWEEN_DETECTION_ATTEMPTS = (ONE_MINUTE / MIN_BPM) * 2.1
+
+interface Args {
+  onTempoDetection: (tempo: number) => void
+}
+interface ReturnType {
+  tap: () => void
+  detecting: boolean
+}
+
+const calculateTempo = (events: number[]): number => {
+  const [start] = events.slice(0, 1)
+  const [end] = events.slice(-1)
+  const meanInterval = (end - start) / (events.length - 1)
+
+  return Math.round(ONE_MINUTE / meanInterval)
+}
+
+export const useTapTempoDetection = (args: Args): ReturnType => {
+  const [detecting, setDetecting] = useState(false)
+  const timestamps = useRef<number[]>([])
+  const lastTapTimer = useRef<number>()
+  const timeElapsedSinceDetection = useRef<number>()
+
+  const detectTempo = (): void => {
+    // Taking the last "N" events means the if the user has been clicking
+    // for a while and possibly changing speed, we're able to base our detection
+    // on the most recent events and make a more reliable estimate of the users intention
+    const lastNTimestamps = timestamps.current.slice(-NUM_OF_INTERVALS_FOR_DETECTION)
+    args.onTempoDetection(calculateTempo(lastNTimestamps))
+  }
+
+  const reset = (): void => {
+    timestamps.current = []
+    setDetecting(false)
+  }
+
+  const tap = (): void => {
+    if (!detecting) setDetecting(true)
+
+    timestamps.current.push(Date.now())
+
+    // We can't indefinitely wait for the user to stop tapping before detecting
+    // If we have enough info to detect we should, but allow the user to keep clicking
+    if (timestamps.current.length >= NUM_OF_INTERVALS_FOR_DETECTION) {
+      detectTempo()
+    }
+
+    // Reset/set a timer to detect if the user stops clicking
+    // and make sure we detect, even if we don't have the ideal number of events
+    if (lastTapTimer.current) clearTimeout(lastTapTimer.current)
+    lastTapTimer.current = window.setTimeout(() => {
+      detectTempo()
+      reset()
+    }, MAX_WAIT_AFTER_LAST_TAP)
+
+    // We should also track "time since last detection" so we don't wait
+    // too long to detect if the user is tapping a very slow tempo
+    if (!timeElapsedSinceDetection.current) {
+      timeElapsedSinceDetection.current = window.setTimeout(() => {
+        detectTempo()
+        clearTimeout(timeElapsedSinceDetection.current)
+      }, MAX_WAIT_BETWEEN_DETECTION_ATTEMPTS)
+    }
+  }
+
+  useEffect(() => {
+    // Cleanup any timers on dismount
+    return () => {
+      if (lastTapTimer.current) clearTimeout(lastTapTimer.current)
+      if (timeElapsedSinceDetection.current) {
+        clearTimeout(timeElapsedSinceDetection.current)
+      }
+    }
+  }, [])
+
+  return { tap, detecting }
+}
--- src/components/metronome/metronome.tsx
+++ src/components/metronome/metronome.tsx
@@ -2,11 +2,18 @@ import { FC, KeyboardEvent, useState } from 'react'
 import { joinClassNames } from '@/utils'
 import { ControlWheel } from '@/components/common/ControlWheel'
 import { DEFAULT_BPM, MAX_BPM, METRONOME_ITEMS, MIN_BPM } from './Metronome.config'
+import { useTapTempoDetection } from './hooks/useTapTempoDetection'
 import styles from './Metronome.module.scss'
 
 export const Metronome: FC = () => {
   const [bpm, setBpm] = useState<number>(DEFAULT_BPM)
   const [playing, setPlaying] = useState(false)
+
+  const { tap, detecting } = useTapTempoDetection({ onTempoDetection: setBpm })
+
+  const togglePlaying = (): void => {
+    setPlaying(!playing)
+  }
 
   const handleKeyBoardEvent = (event: KeyboardEvent): void => {
     if (event.key === 'ArrowDown') {
@@ -15,19 +22,23 @@ export const Metronome: FC = () => {
     if (event.key === 'ArrowUp') {
       setBpm((bpm) => Math.min(bpm + 1, MAX_BPM))
     }
-  }
-
-  const togglePlaying = (): void => {
-    setPlaying(!playing)
+    if (event.key === 'SpaceBar') {
+      togglePlaying()
+    }
   }
 
   return (
-    <div className={styles.wrapper} onKeyUp={handleKeyBoardEvent} tabIndex={0}>
-      <div className={styles.bpm} onKeyUp={handleKeyBoardEvent}>
+    <div className={styles.wrapper} onKeyDown={handleKeyBoardEvent} tabIndex={0}>
+      <div className={styles.bpm}>
         <div className={styles.bpmDisplay}>
           <ControlWheel items={METRONOME_ITEMS} selectedItem={bpm} onSelect={setBpm} />
         </div>
         <span>BPM</span>
+
+        <button onClick={tap}>
+          <h4>Tap</h4>
+          {detecting && 'Detecting...'}
+        </button>
       </div>
 
       <button
