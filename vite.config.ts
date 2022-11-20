f3dec72eb4d76ea7e4374836e98205506a8da145
William Chadwick <william_chadwick@external.mckinsey.com>
2022-11-20 13:36:31 +0000

    feat: add modules resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   css: {
     postcss: {
       plugins: [autoprefixer],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/index.scss";`,
+      },
     },
   },
 })
