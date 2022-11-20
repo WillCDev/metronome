5a2d17201c58d0b0c37488a74b6b7a7c27dd73d3
William Chadwick <william_chadwick@external.mckinsey.com>
2022-11-20 15:52:00 +0000

    feat: add keyboard navigation to control BPM changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react'
 import { Metronome } from '@/components/metronome'
 import styles from './App.module.scss'
 
-const App: FC = () => {
-  return (
-    <main className={styles.wrapper}>
-      <Metronome />
-    </main>
-  )
-}
+const App: FC = () => (
+  <main className={styles.wrapper}>
+    <Metronome />
+  </main>
+)
 
 export default App
--- src/components/metronome/metronome.tsx
+++ src/components/metronome/metronome.tsx
@@ -1,14 +1,23 @@
-import { FC, useState } from 'react'
+import { FC, KeyboardEvent, useState } from 'react'
 import { ControlWheel } from '../common/ControlWheel'
-import { DEFAULT_BPM, METRONOME_ITEMS } from './Metronome.config'
+import { DEFAULT_BPM, MAX_BPM, METRONOME_ITEMS, MIN_BPM } from './Metronome.config'
 import styles from './Metronome.module.scss'
 
 export const Metronome: FC = () => {
   const [bpm, setBpm] = useState<number>(DEFAULT_BPM)
 
+  const handleKeyBoardEvent = (event: KeyboardEvent): void => {
+    if (event.key === 'ArrowDown') {
+      setBpm((bpm) => Math.max(bpm - 1, MIN_BPM))
+    }
+    if (event.key === 'ArrowUp') {
+      setBpm((bpm) => Math.min(bpm + 1, MAX_BPM))
+    }
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.bpm}>
+    <div className={styles.wrapper} onKeyUp={handleKeyBoardEvent} tabIndex={0}>
+      <div className={styles.bpm} onKeyUp={handleKeyBoardEvent}>
         <div className={styles.bpmDisplay}>
           <ControlWheel items={METRONOME_ITEMS} selectedItem={bpm} onSelect={setBpm} />
         </div>
