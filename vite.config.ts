368c3c6cc2972a45258fccb4f58b858636577f4e
William Chadwick <william_chadwick@external.mckinsey.com>
2022-11-22 12:16:16 +0000

    build: use tsconfig as source of truth for path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [tsconfigPaths(), react()],
   css: {
     postcss: {
       plugins: [autoprefixer],
